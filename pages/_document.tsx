3bc6a9704207d552733994d9efe85f900283fc95
hariprasadkc <kcappu@gmail.com>
2025-10-31 16:33:47 +0530

    change logo path, added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html className="light">
-            <Head />
+            <Head>
+                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+                <link rel="alternate icon" href="/favicon.ico" />
+            </Head>
             <body className="dark:bg-gray-800">
                 <Main />
                 <NextScript />
